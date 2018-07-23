a2694b57211dacc5f785800645837faf7fb5dc07
stephane23 <stephane.fernandes@et.simplon-roanne.com>
2018-07-23 12:25:33 +0200

    merde

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+//exo1
+$name = "Stef <br>";
+echo $name; 
+
+//exo2
+$lastname = "fernandes <br>";
+$firstname = "stephane <br>";
+$age = "33 <br>";
+echo $lastname, $firstname, $age;
+
+//exo3
+$km = 1;
+echo $km."<br>";
+$km = 3;
+echo $km."<br>";
+$km = 125;
+echo $km."<br>";
+
+//exo4
+$string = "joseph";
+$number = 23;
+$decimal = 23.3;
+$bool = true;
+echo $string."<br>", $number."<br>", $decimal."<br>", $bool."<br>";
+
+//exo5
+$numero = NULL;
+echo $numero;
+$numero = 3;
+echo $numero."<br>";
+
+//exo6
+$name = "Stef";
+echo "Bonjour $name, comment vas tu?.<br>";
+
+//exo7
+$lastname = "fernandes";
+$firstname = "stephane";
+$age = 33;
+echo "Bonjour " .$lastname ." " .$firstname .", tu as " .$age ." ans.<br>";
+
+//exo8
+$addition = 3 + 4;
+echo $addition."<br>";
+$multiplication = 5 * 20;
+echo $multiplication."<br>";
+$division = 45 / 5;
+echo $division."<br>";
